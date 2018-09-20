ad28d7a93d21bc79242f3e6bf73df72b57111322
JRob <jeremy.robertou@gmail.com>
2018-09-20 10:08:37 +0200

    testing params

--- src/main/java/fr/cgi/sftp/Sftp.java
+++ src/main/java/fr/cgi/sftp/Sftp.java
@@ -16,6 +16,7 @@
 
 package fr.cgi.sftp;
 
+import io.vertx.core.logging.LoggerFactory;
 import net.schmizz.sshj.SSHClient;
 import net.schmizz.sshj.sftp.SFTPClient;
 import net.schmizz.sshj.userauth.keyprovider.KeyProvider;
@@ -38,9 +39,12 @@ public class Sftp extends BusModBase implements Handler<Message<JsonObject>> {
 	private final String PORT = "port";
 	private final String USERNAME = "username";
 	private final String PASSWORD = "password";
+	private final String PASSPHRASE = "passphrase";
 	private final String SSHKEY = "sshkey";
 	private final String LOCAL_FILE = "local-file";
 	private final String DIST_FILE = "dist-file";
+	private Logger log = LoggerFactory.getLogger(Sftp.class);
+
 
 	@Override
 	public void start() {
@@ -71,8 +75,15 @@ public class Sftp extends BusModBase implements Handler<Message<JsonObject>> {
 		SSHClient ssh = new SSHClient();
 		SFTPClient sftp = null;
 		JsonObject params = message.body();
+		logger.info("KNOWN_HOSTS " + params.getString(KNOWN_HOSTS));
+		logger.info("HOSTNAME " + params.getString(HOSTNAME));
+		logger.info("USERNAME " + params.getString(USERNAME));
+		logger.info("PORT " + params.getInteger(PORT));
+		logger.info("SSHKEY " + params.getString(SSHKEY));
+		logger.info("PASSPHRASE " + params.getString(PASSPHRASE));
+		logger.info("LOCAL_FILE " + params.getString(LOCAL_FILE));
+		logger.info("DIST_FILE " + params.getString(DIST_FILE));
 		if(validateParams(message)) {
-
 			try {
 				ssh.loadKnownHosts(new File(params.getString(KNOWN_HOSTS)));
 				if (params.containsKey(PORT)) {
@@ -83,7 +94,13 @@ public class Sftp extends BusModBase implements Handler<Message<JsonObject>> {
 				boolean connected = false;
 				try {
 					if (params.containsKey(SSHKEY)) {
-						KeyProvider kp = ssh.loadKeys(params.getString(SSHKEY));
+						KeyProvider kp;
+						if (params.containsKey(PASSPHRASE)) {
+							kp = ssh.loadKeys(params.getString(SSHKEY), params.getString(PASSPHRASE));
+						}
+						else {
+							kp = ssh.loadKeys(params.getString(SSHKEY));
+						}
 						List<KeyProvider> kplist = new LinkedList<>();
 						kplist.add(kp);
 						ssh.authPublickey(params.getString(USERNAME), kplist);
@@ -91,6 +108,7 @@ public class Sftp extends BusModBase implements Handler<Message<JsonObject>> {
 					}
 				} catch (Exception e) {
                     logger.error("Error when connecting with SSH Key ", e);
+					throw e;
 				}
 				if (!connected && params.containsKey(PASSWORD)) {
 					ssh.authPassword(params.getString(USERNAME), params.getString(PASSWORD));
@@ -128,29 +146,32 @@ public class Sftp extends BusModBase implements Handler<Message<JsonObject>> {
 	 */
 	private boolean validateParams(Message<JsonObject> message) {
 		JsonObject params = message.body();
-
-		if( params.getString(KNOWN_HOSTS, "").isEmpty() ) {
+		if( params == null ){
+			sendError(message, " params is required");
+			return false;
+		}
+		if( params.getString(KNOWN_HOSTS, "") != null && params.getString(KNOWN_HOSTS, "").isEmpty() ) {
 			sendError(message, KNOWN_HOSTS + " absent");
 			return false;
 		}
-		if( params.getString(HOSTNAME, "").isEmpty() ) {
+		if( params.getString(HOSTNAME, "") != null && params.getString(HOSTNAME, "").isEmpty() ) {
 			sendError(message, HOSTNAME + " absent");
 			return false;
 		}
-		if( params.getString(USERNAME, "").isEmpty() ) {
+		if( params.getString(USERNAME, "") != null && params.getString(USERNAME, "").isEmpty() ) {
 			sendError(message, USERNAME + " absent");
 			return false;
 		}
-		if( params.getString(PASSWORD, "").isEmpty()
-				&& params.getString(SSHKEY, "").isEmpty() ) {
+		if( params.getString(PASSWORD, "") != null && params.getString(PASSWORD, "").isEmpty()
+			 	&& params.getString(SSHKEY, "").isEmpty() ) {
 			sendError(message, PASSWORD + " and " + SSHKEY + " absent");
 			return false;
 		}
-		if( params.getString(LOCAL_FILE, "").isEmpty() ) {
+		if( params.getString(LOCAL_FILE, "") != null && params.getString(LOCAL_FILE, "").isEmpty() ) {
 			sendError(message, LOCAL_FILE + " absent");
 			return false;
 		}
-		if( params.getString(DIST_FILE, "").isEmpty() ) {
+		if( params.getString(DIST_FILE, "") != null && params.getString(DIST_FILE, "").isEmpty() ) {
 			sendError(message, DIST_FILE + " absent");
 			return false;
 		}
@@ -160,5 +181,4 @@ public class Sftp extends BusModBase implements Handler<Message<JsonObject>> {
 		}
 		return true;
 	}
-
 }
